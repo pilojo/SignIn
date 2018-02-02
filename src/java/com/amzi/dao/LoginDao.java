14cee5ba09b87ac0b2b9cf88db37b6a9bd9ea22b
pilojo <jp9723@hotmail.com>
2018-02-02 16:59:14 -0500

    First commit

---
+++ src/java/com/amzi/dao/LoginDao.java
@@ -0,0 +1,67 @@
+package com.amzi.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+//User password for database: $2a$12$XAWhWQDN7i9lJz93iGBace3jwrAdcmt2mDYJIQbELp43piYh8iXlC
+
+public class LoginDao {
+    public static boolean validate(String name, String pass) {        
+        boolean status = false;
+        Connection conn = null;
+        PreparedStatement pst = null;
+        ResultSet rs = null;
+
+        String url = "jdbc:mysql://localhost:3306/";
+        String dbName = "form";
+        String driver = "com.mysql.jdbc.Driver";
+        String userName = "root";
+        String password = "mainstreet";
+        try {
+            Class.forName(driver).newInstance();
+            conn = DriverManager
+                    .getConnection(url + dbName, userName, password);
+
+            pst = conn
+                    .prepareStatement("select password from login where user=?");
+            pst.setString(1, name);
+
+            rs = pst.executeQuery();
+            if(rs.next())
+            {
+	            String result = rs.getString("password");
+	            status = result.equals(pass);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e);
+        } finally {
+            if (conn != null) {
+                try {
+                    conn.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (pst != null) {
+                try {
+                    pst.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (rs != null) {
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return status;
+    }
+}
